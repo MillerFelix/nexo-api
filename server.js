5333822477e025419bb411231dc196087214f43b
MillerFelix <millerredigolo@gmail.com>
2026-04-16 15:56:10 -0300

    Liberando o CORS

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const pool = require("./database");
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
